f8d1be9b1798f6f59b88e98a124634a0b81441ec
kuailewangzi1212 <115504218@qq.com>
2016-02-26 21:11:57 +0800

    user center

---
+++ backend/SysCommon/JSONDAL/OperateDataJsonHelper.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+using Newtonsoft.Json.Linq;
+namespace ws.data.jsonDal
+{   
+    /// <summary>
+    /// json处理类
+    /// </summary>
+    public class OperateDataJsonHelper
+    {
+        /// <summary>
+        /// 任意已经存在的defxml文件名用于拼装defid
+        /// </summary>
+        public string defid = PMS.Sys.CSystem.baseSystem.str_dbDefault;//并没有真实的用到
+        //string pstda = "{ defid: '{0}', fmtid: 'update', strparam: {1}, dStyle: 'xml' };";
+
+        /// <summary>
+        /// 后台的defxml数据配置文件，如果是自动生成的则用db_test.sys_employee【数据库定义xml.表名】
+        /// </summary>
+        public ArrayList tblList=new ArrayList();//重要
+
+        public Dictionary<string, JObject> dic_data = new Dictionary<string, JObject>();
+
+        public OperateDataJsonHelper()
+        {
+            if (defid == "")
+            {
+                defid = "db_drsyn";
+            }
+        }
+
+       
+        /// <summary>
+        /// 生成insert、update、delete Json数据
+        /// </summary>
+        /// <param name="xmldef"></param>
+        /// <param name="strUpdJson">添加或者修改的json{a:'aaa',b:'bb'}或者[{a:'aaa',b:'bb'},{a:'aaa1',b:'bb2'}]；修改时是{a:'aaa',b:'bb',key_iid:'1'}或[{a:'aaa',b:'bb'},{a:'aaa1',b:'bb2',key_iid:'2'}]</param>
+        /// <param name="strDelJson">删除的json["10","11"]或{"bill_code":"201201010001"}或者{"where_1":"input_dt>='20120101'"}</param>
+        /// <param name="flag">类型:insert、update、delete</param>
+        public void Gen_Data(string xmldef,string strUpdJson,string flag,string strDelJson)
+        {
+            #region 
+            
+            try
+            {
+                //参数初始化
+                string c0 = "8";
+                if (flag.ToLower() == "insert")
+                {
+                    c0 = "8";
+                }
+                if (flag.ToLower() == "update")
+                {
+                    c0 = "9";
+                }
+
+                //1、判断是否存在
+                if (tblList.Contains(xmldef) == false)
+                {
+                    dic_data.Add(xmldef, JObject.Parse("{}"));
+                    tblList.Add(xmldef);
+                }
+                //2、取出表对应的原有Json数据
+                //"db_test.sys_employee":
+                //{ data: [{ c0: "8", name: $("#name").val() }], //新增
+                //    delData: []
+                //}
+                JObject obj_xmldef = dic_data[xmldef];
+
+                //3、取出obj_xmldef的data部分            
+                JArray arr_data = JArray.Parse((obj_xmldef["data"] ?? "[]").ToString());
+
+                //4、添加行记录到数组中
+                if (strUpdJson.Length > 0)
+                {
+                    if (strUpdJson.Substring(0, 1) == "{")//如果只有一条记录
+                    {
+                        JObject jobj_one = JObject.Parse(strUpdJson);                        
+                        jobj_one.AddFirst(new JProperty("c0", c0));
+                        //jobj_one["c0"] = c0;
+                        /*
+                        if (c0 == "9")//修改                       
+                        {
+                            string iid = (jobj_one["iid"] ?? "").ToString();
+                            if (iid == "")
+                            {
+                                throw new Exception("iid 不能为空!");
+                            }
+                            jobj_one.Remove("iid");
+                            jobj_one["key_iid"] = iid;
+                        }*/
+                        arr_data.Add(jobj_one);
+                    }
+                    else
+                    {
+                        JArray jarr_muti = JArray.Parse(strUpdJson);
+                        for (int i = 0; i < jarr_muti.Count; i++)
+                        {
+                            JObject jobj_one = JObject.Parse(jarr_muti[i].ToString());
+                            jobj_one.AddFirst(new JProperty("c0", c0));
+                            //jobj_one["c0"] = c0;
+                            /*
+                            if (c0 == "9")//修改                                               
+                            {
+                                string iid = (jobj_one["iid"] ?? "").ToString();
+                                if (iid == "")
+                                {
+                                    throw new Exception("iid 不能为空!");
+                                }
+                                jobj_one.Remove("iid");
+                                jobj_one["key_iid"] = iid;
+                            }*/
+                            arr_data.Add(jobj_one);
+                        }
+                    }
+                }
+
+                //5、将处理后的数组赋值给原有数据
+                obj_xmldef["data"] = arr_data;                
+                //6、处理删除数据                
+                if (strDelJson.Length > 0)
+                {
+                    if (strDelJson.Substring(0, 1) == "[")//数组
+                    {
+                        obj_xmldef["delData"] = JArray.Parse(strDelJson);   
+                    }
+                    if (strDelJson.Substring(0, 1) == "{")//数组
+                    {
+                        obj_xmldef["delData"] = JObject.Parse(strDelJson);   
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            #endregion
+        }
+
+
+        /// <summary>
+        /// 生成insert json
+        /// </summary>
+        /// <param name="xmldef"></param>
+        /// <param name="json">{a:'aaa',b:'bb'}或者[{a:'aaa',b:'bb'},{a:'aaa1',b:'bb2'}]</param>
+        /// <param name="msg"></param>
+        public void Gen_Insert(string xmldef,string json,ref string msg)
+        {
+            try
+            {
+                Gen_Data(xmldef, json, "insert", "");
+            }
+            catch(Exception ex)
+            {
+                msg = ex.Message;
+            }
+        }
+        /// <summary>
+        /// 生成update json
+        /// </summary>
+        /// <param name="xmldef"></param>
+        /// <param name="json">{'key_iid':1,a:'aaa',b:'bb'}或者[{'key_iid':1,a:'aaa',b:'bb'},{'key_iid':2,a:'aaa1',b:'bb2'}]</param>
+        /// <param name="msg"></param>
+        public void Gen_Update(string xmldef, string json, ref string msg)
+        {
+            try
+            {
+                Gen_Data(xmldef, json, "update", "");
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xmldef"></param>
+        /// <param name="json">["10","11"]或{"bill_code":"201201010001"}或者{"where_1":"input_dt>='20120101'"}</param>
+        /// <param name="msg"></param>
+        public void Gen_Delete(string xmldef, string json, ref string msg)
+        {
+            try
+            {
+                Gen_Data(xmldef, "", "delete", json);
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// 生成最终的{ defid: '{0}', fmtid: 'update', strparam: '....', dStyle: 'xml' }
+        /// </summary>
+        /// <returns></returns>
+        public string GenOperateJson()
+        {            
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{ \"tblList\": [");
+            foreach (string tb in tblList)
+            {
+                sb.AppendFormat("\"{0}\",",tb);
+            }
+
+            sb.Remove(sb.Length - 1, 1);//移除最后一个符号","
+            sb.Append("],");
+            foreach (string tb in tblList)
+            {
+                sb.AppendFormat("\"{0}\": ",tb);
+                sb.Append(dic_data[tb].ToString());
+                sb.Append(",");
+            }
+            sb.Remove(sb.Length - 1, 1);//移除最后一个符号","
+                        
+            sb.Append("}");
+
+            string pstda = string.Format("{{ \"defid\": \"{0}\", \"fmtid\": \"update\", \"strparam\":'{1}', \"dStyle\": \"xml\" }}", this.defid,sb.ToString());
+
+            return pstda;
+        }
+    }
+}
---
+++ backend/appweb/handler/Login.ashx
@@ -0,0 +1,89 @@
+﻿<%@ WebHandler Language="C#" Class="Login" %>
+
+using System;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.IO;
+public class Login : IHttpHandler, System.Web.SessionState.IRequiresSessionState 
+{
+
+
+    private HttpContext _context;
+    public void ProcessRequest (HttpContext context) {
+        _context = context;
+        string ls_op = context.Request.Form["op"].ToString();
+        string ls_rtn = "";
+        if (ls_op.ToLower() == "send_checkcode")
+        {
+            string ls_phone = context.Request.Form["phone"].ToString();
+            ls_rtn=SendCheckCode(ls_phone);          
+        }
+        context.Response.Write(ls_rtn);
+    }
+
+    public string SendCheckCode(string phone)
+    {
+        string ls_rtn = "";
+        string s_phone = phone;
+        string s_number = GetRandomString(5);//产生随机数
+        string s_code = GetRandomString(5);//产生随机数
+        _context.Session[phone] = s_code;//将手机号和随机数放置到session中，便于第二次提交注册数据时验证        
+        try
+        {
+
+            //发送短信息信息到指定的手机
+            SendSMS(s_phone, "【科腾社区】注册,验证码" + s_code + ",短信编号" + s_number + "。");            
+        }
+        catch (Exception ex)
+        {
+            ls_rtn = ex.Message;              
+        }
+        return ls_rtn;
+    }
+
+    public static void SendSMS(string phonenumber, string content)
+    {
+        string strURL = "http://www.ztsms.cn/sendSms.do?username=keteng&password=6IDgPhVY&mobile=" + phonenumber + "&content=" + content + "&dstime=&productid=676767&xh=";
+        WebRequest wRequest = WebRequest.Create(strURL);
+        WebResponse wResponse = wRequest.GetResponse();
+        Stream stream = wResponse.GetResponseStream();
+        StreamReader reader = new StreamReader(stream, System.Text.Encoding.Default);
+        string r = reader.ReadToEnd();
+        wResponse.Close();
+    }
+
+    /// 生成8位随机数
+    /// </summary>
+    /// <param name="length"></param>
+    /// <returns></returns>
+    static public string GetRandomString(int len)
+    {
+        //string s = "123456789abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ";
+        string s = "1234567890";
+        string reValue = string.Empty;
+        Random rnd = new Random(getNewSeed());
+        while (reValue.Length < len)
+        {
+            string s1 = s[rnd.Next(0, s.Length)].ToString();
+            if (reValue.IndexOf(s1) == -1) reValue += s1;
+        }
+        return reValue;
+    }
+
+    private static int getNewSeed()
+    {
+        byte[] rndBytes = new byte[4];
+        System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+        rng.GetBytes(rndBytes);
+        return BitConverter.ToInt32(rndBytes, 0);
+    }     
+
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
