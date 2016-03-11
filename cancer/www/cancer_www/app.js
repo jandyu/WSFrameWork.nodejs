9088f7d3aec87d090e2ff84ca2acb87e00a0f878
jandyu <jandyu@gmail.com>
2016-03-11 13:11:43 +0800

    js css page template integration

--- cancer/www/cancer_www/app.js
+++ cancer/www/cancer_www/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
-var logger = require('morgan');
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -10,12 +10,25 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
+var adaro = require('adaro');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var options = {
+    cache:false,
+    whitespace:true,
+    helpers: [
+        'dustjs-helpers' //installed modules
+    ]
+};
+app.engine('dust', adaro.dust(options));
+app.set('view engine', 'dust');
+
+
 
 app.use(favicon());
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
