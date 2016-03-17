31c4c0438246ced78b6b57a19a44f576d0492049
jandyu <jrain@Jrain-MBP.local>
2016-03-17 22:00:53 +0800

    reformat code

--- cancer/www/cancer_www/routes/index.js
+++ cancer/www/cancer_www/routes/index.js
@@ -4,9 +4,9 @@ var lifestar = require("../lib/lifestar");
 var logger = require("../lib/log").sqllog;
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  logger.info(lifestar.resource.data.homepage);
-  res.render('index', lifestar.resource.data.homepage);
+router.get('/', function (req, res) {
+    logger.info(lifestar.resource.data.homepage);
+    res.render('index', lifestar.resource.data.homepage);
 });
 
 module.exports = router;
